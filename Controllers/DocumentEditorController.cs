add1fd2cba2170b6a10eceb94725af4f136d569e
Harish <harish.7080@gmail.com>
2019-07-01 23:10:04 +0530

    Add project files.

---
+++ Controllers/DocumentEditorController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Syncfusion.EJ2.DocumentEditor;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using Microsoft.AspNetCore.Cors;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    public class DocumentEditorController : Controller
+    {
+        [AcceptVerbs("Post")]
+        [HttpPost]
+        [EnableCors("AllowAllOrigins")]
+        [Route("Import")]
+
+        public string Import(IFormCollection data)
+        {
+            if (data.Files.Count == 0)
+                return null;
+            Stream stream = new MemoryStream();
+            IFormFile file = data.Files[0];
+            int index = file.FileName.LastIndexOf('.');
+            string type = index > -1 && index < file.FileName.Length - 1 ?
+                file.FileName.Substring(index) : ".docx";
+            file.CopyTo(stream);
+            stream.Position = 0;
+
+            WordDocument document = WordDocument.Load(stream, GetFormatType(type.ToLower()));
+            string sfdt = Newtonsoft.Json.JsonConvert.SerializeObject(document);
+            document.Dispose();
+            return sfdt;
+        }
+
+        internal static FormatType GetFormatType(string format)
+        {
+            if (string.IsNullOrEmpty(format))
+                throw new NotSupportedException("EJ2 DocumentEditor does not support this file format.");
+            switch (format.ToLower())
+            {
+                case ".dotx":
+                case ".docx":
+                case ".docm":
+                case ".dotm":
+                    return FormatType.Docx;
+                case ".dot":
+                case ".doc":
+                    return FormatType.Doc;
+                case ".rtf":
+                    return FormatType.Rtf;
+                case ".txt":
+                    return FormatType.Txt;
+                case ".xml":
+                    return FormatType.WordML;
+                default:
+                    throw new NotSupportedException("EJ2 DocumentEditor does not support this file format.");
+            }
+        }
+
+    }
+}
